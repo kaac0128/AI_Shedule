4728f0e521d873a26a0e8e0720afb96155a7b7a2
DUVAN SARMIENTO S <dsarmiento710@misena.edu.co>
2024-04-24 21:54:33 -0500

    :sparkles: develop: Login form part 1

--- frontend/src/Login/components/Login.jsx
+++ frontend/src/Login/components/Login.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import './Login.css';
 
 export default function Login() {
     const [loginForm, setLoginForm] = useState({
@@ -10,22 +11,24 @@ export default function Login() {
         setLoginForm({...loginForm, [e.target.name]: e.target.value});
     }
 
-    const onClick = () => {
+    const onSubmit = (e) => {
+        e.preventDefault();
+
         alert(`User: ${loginForm.user} and Password: ${loginForm.password}`);
     }
 
     return (
-        <form>
+        <form className={"login-container-form"} onSubmit={onSubmit}>
+            <div className={"login-content-input"}>
+                <label className={"login-label"}>User</label>
+                <input className={"login-input"} type={"text"} value={loginForm.user || ""} name={"user"} placeholder={"Username"} onChange={onChangeLoginForm}/>
+            </div>
+            <div className={"login-content-input"}>
+                <label className={"login-label"}>Password</label>
+                <input className={"login-input"} type={"text"} value={loginForm.password || ""} name={"password"} placeholder={"Password"} onChange={onChangeLoginForm}/>
+            </div>
             <>
-                <label>User</label>
-                <input type={"text"} name={"user"} onChange={onChangeLoginForm}/>
-            </>
-            <>
-                <label>Password</label>
-                <input type={"text"} name={"password"} onChange={onChangeLoginForm}/>
-            </>
-            <>
-                <button onClick={onClick}>Sing in</button>
+                <button className={"send-button"} type={"submit"}>Sing in</button>
             </>
         </form>
     );
